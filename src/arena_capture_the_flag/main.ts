61129999da25dde37abed067874abe3fbbd4baa4
dmcclung <35938+dmcclung@users.noreply.github.com>
2024-01-02 17:34:38 -0600

    split into teams

--- src/arena_capture_the_flag/main.ts
+++ src/arena_capture_the_flag/main.ts
@@ -29,12 +29,11 @@
 
 // This stuff is arena-specific
 import { ATTACK, HEAL, RANGED_ATTACK } from "game/constants";
-import { Creep, GameObject } from "game/prototypes";
+import { Creep, GameObject, Id, RoomPosition} from "game/prototypes";
 import { getDirection, getObjectsByPrototype, getRange, getTicks } from "game/utils";
 import { Flag, BodyPart } from "arena";
 import { Visual } from "game/visual";
 import { searchPath } from "game/path-finder";
-import { setUncaughtExceptionCaptureCallback } from "process";
 
 declare module "game/prototypes" {
   interface Creep {
@@ -64,11 +63,31 @@ declare module "game/prototypes" {
 // The game guarantees there will be no global reset during the match.
 // Note that you cannot assign any game objects here, since they are populated on the first tick, not when the script is initialized.
 let myCreeps: Creep[];
+
+let attackerCount: number = 0;
+const MAX_ATTACKERS: number = 1;
+
+let rangedCount: number = 0;
+const MAX_RANGED: number = 3;
+
+let healerCount: number = 0;
+const MAX_HEALERS: number = 3;
+
+let redTeam: Id<Creep>[] = [];
+let blueTeam: Id<Creep>[] = [];
+let teamInit: Boolean;
+
 let enemyCreeps: Creep[];
 let bodyParts: BodyPart[];
-let scout: Creep;
-let scoutReachedFirstCamp: Boolean = false;
-let runners: Creep[];
+
+let redTeamScout: Creep;
+let blueTeamScout: Creep;
+
+let redTeamRendevouz: RoomPosition = {x: 68, y: 37}
+let redTeamReachedRendevouz: Id<Creep>[] = [];
+let blueTeamRendevouz: RoomPosition = {x: 37, y: 68}
+let blueTeamReachedRendevouz: Id<Creep>[] = [];
+
 let enemyFlag: Flag | undefined;
 
 // This is the only exported function from the main module. It is called every tick.
@@ -82,38 +101,78 @@ export function loop(): void {
   
   // [{"id":"bodyPart30","x":39,"y":61,"ticksToDecay":99,"type":"move"}]
   bodyParts = getObjectsByPrototype(BodyPart);
-  console.log('BodyParts: ', JSON.stringify(bodyParts));
 
   // Notice how getTime is a global function, but not Game.time anymore
   if (getTicks() % 10 === 0) {
     console.log(`I have ${myCreeps.length} creeps`);
   }
 
-  // Split up into two pods, where each pod has a melee cree
-  
+  // Split creeps into two teams to defend the river crossings
+  if (!teamInit) {
+    myCreeps.forEach(creep => {
+      if (isAttacker(creep)) {
+        assignToTeam(creep, attackerCount, MAX_ATTACKERS)
+        attackerCount++
+      }
+
+      else if (isRanged(creep)) {
+        assignToTeam(creep, rangedCount, MAX_RANGED)
+        rangedCount++
+      }
+
+      else if (isHealer(creep)) {
+        assignToTeam(creep, healerCount, MAX_HEALERS)
+        healerCount++
+      }
+    })
+    teamInit = true;
+  }
 
   // Run all my creeps according to their bodies
   myCreeps.forEach(creep => {
-    if (creep.body.some(i => i.type === ATTACK)) {
+    if (isAttacker(creep)) {
       meleeAttacker(creep);
     }
-    if (creep.body.some(i => i.type === RANGED_ATTACK)) {
+    if (isRanged(creep)) {
       rangedAttacker(creep);
     }
-    if (creep.body.some(i => i.type === HEAL)) {
-      if (!scout) {
-        scout = creep
+    if (isHealer(creep)) {
+      // If our scout is eliminated, replace it
+      if (redTeam.includes(creep.id) && (!redTeamScout || !myCreeps.includes(redTeamScout))) {
+        redTeamScout = creep
+      } else if (blueTeam.includes(creep.id) && (!blueTeamScout || !myCreeps.includes(blueTeamScout))) {
+        blueTeamScout = creep
       }
       healer(creep);
     }
   });
 }
 
+function assignToTeam(creep: Creep, roleCount: number, maxRoleCount: number) {
+  if (!redTeam.includes(creep.id) && !blueTeam.includes(creep.id) && roleCount < maxRoleCount) {
+    redTeam.push(creep.id)
+  } else {
+    blueTeam.push(creep.id)
+  }
+}
+
+function isAttacker(creep: Creep) {
+  return creep.body.some(i => i.type === ATTACK)
+}
+
+function isRanged(creep: Creep) {
+  return creep.body.some(i => i.type === RANGED_ATTACK)
+}
+
+function isHealer(creep: Creep) {
+  return creep.body.some(i => i.type === HEAL)
+}
+
 function meleeAttacker(creep: Creep) {
   // Here is the alternative to the creep "memory" from Screeps World. All game objects are persistent. You can assign any property to it once, and it will be available during the entire match.
-  if (!creep.initialPos) {
-    creep.initialPos = { x: creep.x, y: creep.y };
-  }
+  // if (!creep.initialPos) {
+  //   creep.initialPos = { x: creep.x, y: creep.y };
+  // }
 
   new Visual().text(
     creep.hits.toString(),
@@ -126,14 +185,36 @@ function meleeAttacker(creep: Creep) {
     }
   );
   const targets = enemyCreeps
-    .filter(i => getRange(i, creep.initialPos) < 10)
+    .filter(i => getRange(i, creep) < 10)
     .sort((a, b) => getRange(a, creep) - getRange(b, creep));
 
   if (targets.length > 0) {
     creep.moveTo(targets[0]);
     creep.attack(targets[0]);
   } else {
-    creep.moveTo(creep.initialPos);
+    moveToGoal(creep)
+  }
+}
+
+function moveToGoal(creep: Creep) {
+  let reachedRendevouz, rendevouz
+  if (redTeam.includes(creep.id)) {
+    reachedRendevouz = redTeamReachedRendevouz
+    rendevouz = redTeamRendevouz
+  } else {
+    reachedRendevouz = blueTeamReachedRendevouz
+    rendevouz = blueTeamRendevouz
+  }
+
+  if (reachedRendevouz.includes(creep.id)) {
+    if (enemyFlag) {
+      creep.moveTo(enemyFlag)
+    }
+  } else {
+    creep.moveTo(rendevouz)
+    if (getRange(creep, rendevouz) < 3) {
+      reachedRendevouz.push(creep.id)
+    }
   }
 }
 
@@ -141,51 +222,37 @@ function rangedAttacker(creep: Creep) {
   const targets = enemyCreeps.sort((a, b) => getRange(a, creep) - getRange(b, creep));
 
   if (targets.length > 0) {
-    creep.rangedAttack(targets[0]);
+    const range = 10;
+    const targetsInRange = targets.filter(i => getRange(i, creep) < range);
+    creep.rangedAttack(targetsInRange[0]);
   }
 
-  if (enemyFlag) {
-    // creep.moveTo(enemyFlag);
-  }
+  moveToGoal(creep)
+}
 
-  const range = 3;
-  const enemiesInRange = enemyCreeps.filter(i => getRange(i, creep) < range);
-  if (enemiesInRange.length > 0) {
-    flee(creep, enemiesInRange, range);
+function getTeam(creep: Creep): number {
+  if (redTeam.includes(creep.id)) {
+    return 1
   }
+  return 0
 }
 
 function healer(creep: Creep) {
-  const targets = myCreeps.filter(i => i !== creep && i.hits < i.hitsMax).sort((a, b) => a.hits - b.hits);
+  // Healers follow their team
+  const targets = myCreeps.filter(i => i !== creep && i.hits < i.hitsMax && getTeam(i) == getTeam(creep)).sort((a, b) => a.hits - b.hits);
 
   if (targets.length) {
     creep.moveTo(targets[0]);
   } else {
-    if (enemyFlag) {
-      creep.moveTo(enemyFlag);
-    }
+    moveToGoal(creep)
   }
 
-  if (scout == creep) {
+  if (redTeamScout == creep || blueTeamScout == creep) {
     const bodyPartRange = 3
-    const bodyPartsInRange = bodyParts.filter(i => getRange(i, creep) < bodyPartRange);
+    const bodyPartsInRange = bodyParts.filter(i => getRange(i, creep) <= bodyPartRange);
     if (bodyPartsInRange.length > 0) {
       creep.moveTo(bodyPartsInRange[0]);
-    } else if (enemyFlag) {
-        // const path = searchPath(creep.initialPos, {x: 68, y: 37})
-        console.log('Scout reached first camp', scoutReachedFirstCamp);
-        if (scoutReachedFirstCamp) {
-          creep.moveTo(enemyFlag)
-        } else {
-          // Move to spawn point
-          creep.moveTo({x: 68, y: 37})
-        }
-        if (getRange({x: creep.x, y: creep.y}, {x: 68, y: 37}) < 3) {
-          scoutReachedFirstCamp = true
-        }
     }
-  } else if (enemyFlag) {
-    creep.moveTo(enemyFlag);
   }
 
   const healTargets = myCreeps.filter(i => getRange(i, creep) <= 3).sort((a, b) => a.hits - b.hits);
@@ -198,13 +265,14 @@ function healer(creep: Creep) {
     }
   }
 
-  const range = 7;
-  const enemiesInRange = enemyCreeps.filter(i => getRange(i, creep) < range);
-  if (enemiesInRange.length > 0) {
-    flee(creep, enemiesInRange, range);
-  }
+  // const range = 7;
+  // const enemiesInRange = enemyCreeps.filter(i => getRange(i, creep) < range);
+  // if (enemiesInRange.length > 0) {
+  //   flee(creep, enemiesInRange, range);
+  // }
 }
 
+// Not using this right now
 function flee(creep: Creep, targets: GameObject[], range: number) {
   const result = searchPath(
     creep,
